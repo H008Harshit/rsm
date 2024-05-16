d974d47dfe5bec94f02a458aba0e91011676a87f
H008Harshit <harshitmarmat01@gmail.com>
2024-05-16 10:13:24 +0530

    last moment  chnages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,16 @@ import home_screen from "../../public/assets/images/home_screen_banner.png"
 import PhotoLibrary from "@/components/home/PhotoLibrary";
 import { useState } from "react";
 import home_footer from "../../public/assets/images/home-footer.png"
+import Link from "next/link";
+import deen from "../../public/assets/images/deeendayal.jpeg"
+
 export default function Home() {
   const [search,setSearch] = useState('')
   return (
     <div className="mt-[80px] ">
       <BannerCarousel />
       <div className="mt-[50px] mb-8 desktop:px-[10%] px-4 text-center">
-        <div className="text-[35px] font-andora font-bold text-primary_enabled">
+        <div className="text-[35px] font-andora font-bold text-mg_primary">
           Want to explore more brands?
           <br />
         </div>
@@ -58,11 +61,11 @@ export default function Home() {
         />
         <WomenOfSubstance />
         <div className="relative">
-          <video className="w-full  object-cover h-[80vh]"  autoPlay loop>
+          <video className="w-full  object-cover h-[60vh]"  autoPlay loop>
             <source src="http://www.magicka.in/public_doc/b.mp4" />
           </video>
           <div className="absolute desktop:mx-[10%] flex flex-col desktop:flex-row text-white items-center justify-around z-10 top-[10%] desktop:top-[30%]">
-            <video controls>
+            <video controls >
               <source src="https://video.fdel42-1.fna.fbcdn.net/v/t42.9040-2/70735550_2344054849169992_7559830215547944960_n.mp4?_nc_cat=100&ccb=1-7&_nc_sid=55d0d3&efg=eyJybHIiOjM4OCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIiwidmlkZW9faWQiOjQ4OTQ1MDExODUzNjIyN30%3D&_nc_ohc=X5AWysG6ZVMQ7kNvgGEJlCk&rl=388&vabr=216&_nc_ht=video.fdel42-1.fna&edm=AGo2L-IEAAAA&oh=00_AYD9upMxbGzjL5dX0U5wnsjY_zB6H2w9ldEywVM4dp9ymw&oe=66454585"/>
             </video>
             <div className="text-DH2 desktop:w-[40%] mt-8 desktop:mt-0 w-[90%] text-center">
@@ -72,6 +75,16 @@ export default function Home() {
         </div>
 
         <PhotoLibrary />
+        <div className="desktop:px-[10%] bg-[#fff8f8] mt-[5%] py-[5%] px-4 grid-cols-2 grid gap-10">
+          <img className="desktop:col-span-1 col-span-2 h-auto mx-auto w-[80%]" src={deen.src} alt="image"/>  
+          <div className="desktop:col-span-1 text-mg_primary col-span-2 mx-auto desktop:mx-0 text-DH1 w-[70%] mt-2 desktop:mt-8 text-center font-andora">Deendayal Antyodaya Yojana-National Urban Livelihoods Mission (DAY-NULM)</div>
+          <div className="col-span-2 text-DT2 text-center text-grey_dark w-[80%] mx-auto font-lato">
+            To reduce poverty and vulnerability of the urban poor households by enabling them to access gainful self employment and skilled wage employment opportunities,resulting in an appreciable improvement in their livelihoods on a sustainable basis,through building strong grassroots level institutions of the poor.The mission would aim at providing shelters equipped with essential services to the urban homeless in a phased manner. In addition,the mission would also address livelihood concerns of the urban street vendors by facilitating access to suitable spaces,institutional credit,social security and skills to the urban street vendors for accessing emerging market opportunities.
+            <Link href="https://nulm.gov.in/">
+              <div className="rounded-lg mt-4  w-fit mx-auto p-4 border-[1px] border-mg_primary text-mg_primary">Read More</div>
+            </Link>
+          </div>
+        </div>
         <img className="mt-8" src={home_footer.src} alt="footer-banner"/>
       </div>
       {/* <Blog /> */}
